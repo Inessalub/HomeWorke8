5ef14c33f83ebf698a1e483e992016b21d9560f1
unknown <inessalub@gmail.com>
2022-11-14 13:13:51 +0400

    Написать программу возведения числа А в целую стень B

---
+++ example04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int Degree(int numberA, int numberB)
+{
+    if (numberB < 1) return 1;
+    return numberA * (Degree(numberA, numberB - 1));
+}
+
+Console.WriteLine($"Возведение числа {numberA} в степень {numberB}: {Degree(numberA, numberB)}");
