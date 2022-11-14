8ca0e49ee0a868a987fb59fb75a4508e2b199b5d
unknown <inessalub@gmail.com>
2022-11-14 13:00:08 +0400

    Написать программу вычисления функции Аккермана

---
+++ example03/Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать программу вычисления функции Аккермана
+Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) 
+    return AckermannFunction(m - 1, 1);
+    if (m > 0 && n > 0)
+     return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+return AckermannFunction(m, n);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({m},{n}) = {AckermannFunction(m, n)}");
