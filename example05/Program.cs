aef14bba7e6370ed93443bd897d0d347efb24630
unknown <inessalub@gmail.com>
2022-11-14 14:06:32 +0400

    Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих

---
+++ example05/Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем
+
+int a = GetNumber("Введите число a: ");
+int b = GetNumber("Введите число b: ");
+int N = GetNumber("Введите количество раз, сколько раз мы выведем число: ");
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int Metod(int x, int y, int num)
+{
+    if (num == 1) return x;
+    if (num == 2) return y;
+    return Metod(x, y, num - 1) + Metod(x, y, num - 2);
+}
+for (int i = 1; i < N; i++)
+{
+    Console.Write(Metod(a, b, i) + " ");
+}
